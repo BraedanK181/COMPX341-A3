92c166f4810edc3f77002a1acd98052dbc98444b
bk89 <bk89@lab-rg07-20.cms.waikato.ac.nz>
2022-05-19 14:30:16 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -15,6 +15,6 @@ git commit -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
 
 git push -m "braedan181@gmail.com" "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"
 
-npm run start
+#npm run start
 
 #git push -m "braedan181@gmail.com" "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"
